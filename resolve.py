4a2c9edbaf66ada9841f3fa24d85370d783432aa
RiTmm94 <shabalin.kostya2015@yandex.ru>
2024-06-12 21:40:37 +0500

    Add files via upload

---
+++ resolve.py
@@ -0,0 +1,20 @@
+a = 'My Homework'
+print (len(a))
+first = 2
+second = 19
+summa = (first + second)
+diff = (first - second)
+print (summa , diff)
+first1 = 5
+second2 = 32
+third3 = 35
+mean = ((first1 + second2 +third3) / 3)
+print (mean)
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print (second_string + ', ' + first_string)
+a = 3
+b = 7
+c = 1
+f = (a * b) + (a * c)
+print ((f ** 3) / 2)
